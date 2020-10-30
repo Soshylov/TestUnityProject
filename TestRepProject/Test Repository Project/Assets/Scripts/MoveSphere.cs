0cbd5447d320b0224cab5400319b6af73a749b7e
d.soshylov <d.soshylov@pampamstudio.com>
2020-10-30 12:23:29 +0200

    Пофиксил прыжок

--- TestRepProject/Test Repository Project/Assets/Scripts/MoveSphere.cs
+++ TestRepProject/Test Repository Project/Assets/Scripts/MoveSphere.cs
@@ -6,15 +6,23 @@ public class MoveSphere : MonoBehaviour
 {
     [SerializeField] private float force = default;
     private Rigidbody _rb;
+    private bool _state;
 
     void Awake()
     {
         _rb = GetComponent<Rigidbody>();
     }
-
+    private void OnCollisionEnter(Collision collision)
+    {
+        _state = true;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        _state = false;
+    }
     void Update()
     {
-        if (Input.GetKey("space"))
+        if (Input.GetKey("space") && _state )
         {
             _rb.AddForce(Vector3.up * force);
         }
