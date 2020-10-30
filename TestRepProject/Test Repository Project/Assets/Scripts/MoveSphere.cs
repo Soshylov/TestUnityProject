7a7fd23ad1d48f6b036f3755458249d55437fba3
d.soshylov <d.soshylov@pampamstudio.com>
2020-10-30 12:14:40 +0200

    Создал новый проект. Добавил основные папки, обьекты и скрипт на прыжок сферы.

---
+++ TestRepProject/Test Repository Project/Assets/Scripts/MoveSphere.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSphere : MonoBehaviour
+{
+    [SerializeField] private float force = default;
+    private Rigidbody _rb;
+
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("space"))
+        {
+            _rb.AddForce(Vector3.up * force);
+        }
+    }
+}
